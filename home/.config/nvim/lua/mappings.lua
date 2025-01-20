73204a41ba730ba13ad0c27968d750bd8c751a9d
Jonathan M. Hethey <jonathan.hethey@sumup.com>
2025-01-20 10:33:50 +0100

    adds astronvim mappings

---
+++ home/.config/nvim/lua/mappings.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+          ["<C-n"] = { "<cmd>bnext<cr>", desc = "next tab" },
+          ["<C-p"] = { "<cmd>bprevious<cr>", desc = "previous tab" },
+          -- quick save
+          ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+} 
