b974e981ced7c51a187833224cc1846a9b284256
Jonathan M. Hethey <public@jonathanmh.com>
2025-01-30 01:11:08 +0100

    update nvim mappings

--- home/.config/nvim/lua/plugins/mappings.lua
+++ home/.config/nvim/lua/plugins/mappings.lua
@@ -9,10 +9,10 @@ return {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-          ["<C-n"] = { "<cmd>bnext<cr>", desc = "next tab" },
-          ["<C-p"] = { "<cmd>bprevious<cr>", desc = "previous tab" },
+          ["<Cn>"] = { "<cmd>bnext<cr>", desc = "next tab" },
+          ["<Cp>"] = { "<cmd>bprevious<cr>", desc = "previous tab" },
           -- quick save
-          ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+          ["<Cs>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
         },
         t = {
           -- setting a mapping to false will disable it
